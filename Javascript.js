9a3efafd1a4568e5e644e3f2428cc7f9a47e09f6
Shantanu-007 <meshantanu03@gmail.com>
2021-07-31 01:54:33 +0530

    Create Javascript.js

---
+++ Javascript.js
@@ -0,0 +1,40 @@
+let btnAdd = document.querySelector("button");
+let table = document.querySelector("table");
+
+
+
+
+
+
+
+let productInput = document.querySelector("#product")
+let quantityInput = document.querySelector("#quantity")
+let priceInput = document.querySelector("#product")
+let datetimeInput = document.querySelector("#datetime")
+let valueInput = document.querySelector("#value")
+
+
+
+btnAdd.addEventListener ('click', () => {
+    let product = productInput.value;
+    let quantity = quantityInput.value;
+    let price = priceInput.value;
+    let datetime = datetimeInput.value;
+    let value = valueInput.value;
+
+    let template = `
+         <tr>
+            <td>${product}</td>
+            <td>${quantity}</td>
+            <td>${price}</td>
+            <td>${datetime}</td>
+            <td>${value}</td>
+         </tr>`
+
+         table.innerHTML += template;
+        
+    
+});
+
+
+
